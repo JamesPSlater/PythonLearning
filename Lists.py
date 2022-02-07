dd8b38ba390f4521c3467fe71ea99c41fe81cd76
JamesPSlater <jamespaulslater@gmail.com>
2022-02-07 16:17:48 +0000

    Moved function to top.

--- Lists.py
+++ Lists.py
@@ -1,13 +1,15 @@
-list1 = [12, "Hello", 56.44, False, 43, "World", False, 32, False, 7, "Hello", "Hello", "Hello"] #Creating a list
-
-list1.insert(13, True) #inserting into list
-print(list1)
-
 def test(): #Function
     if True in list1:
         print(f"{list1[1]} {list1[5]}")
     else:
         print("This is not right")
+
+list1 = [12, "Hello", 56.44, False, 43, "World", False, 32, False, 7, "Hello", "Hello", "Hello"] #Creating a list
+
+list1.insert(13, True) #inserting into list
+print(list1)
+
+
 test()
 
 # print(set(list1)) #prints unique information
