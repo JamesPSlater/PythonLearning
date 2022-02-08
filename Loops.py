5c4821e1b2094fa5cf54026fdc59f6c5537db823
JamesPSlater <jamespaulslater@gmail.com>
2022-02-08 11:24:26 +0000

    demonstration examples

---
+++ Loops.py
@@ -0,0 +1,26 @@
+'''
+control = 0
+
+while control <= 10:
+    print(f"Control is: {control}")
+    control += 1 #same as 'control = control +1'
+'''
+
+
+'''
+running = True
+
+while running:
+    print("Menu - 1. print hello - 2. print goodbye - 0. exit")
+    input1 = int(input("Please choose an option: "))
+    if input1 == 1:
+        print("hello")
+    elif input1 == 2:
+        print("goodbye")
+    elif input1 == 0:
+        running = False
+    else:
+        print("Not a valid option! Try again.")
+    #Currently dies on not int values
+'''
+
