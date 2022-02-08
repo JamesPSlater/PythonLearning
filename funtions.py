707bd63837e3cca71c886f4989b54f569f52ac80
JamesPSlater <jamespaulslater@gmail.com>
2022-02-08 16:21:33 +0000

    renamed variables

--- funtions.py
+++ funtions.py
@@ -6,29 +6,29 @@
 # print(greet(name, greeting))
 
 
-def grading(hwscore, ascore, exam):
-    percentage = (hwscore + ascore + exam) / 175 * 100
-    return percentage   
+def grading(hScore, aScore, eScore):
+    percentage = (hScore + aScore + eScore) / 175 * 100
+    return percentage    
 
 
 name = input("Please enter a name: ")
-hwscore = int(input("Please enter their homework score: "))
-while hwscore > 25:
+hScore = int(input("Please enter their homework score: "))
+while hScore > 25:
     print("Please enter a max number of 25")
-    hwscore = int(input("Please enter their homework score: "))
+    hScore = int(input("Please enter their homework score: "))
 
 
-ascore = int(input("Please enter their asessment score: "))
-while ascore > 50:
+aScore = int(input("Please enter their asessment score: "))
+while aScore > 50:
     print("Please enter a max number of 50")
-    ascore = int(input("Please enter their asessment score: "))
+    aScore = int(input("Please enter their asessment score: "))
 
-exam = int(input("Please enter their exam score: "))
-while exam > 100:
+eScore = int(input("Please enter their eScore score: "))
+while eScore > 100:
     print("Please enter a max number of 100")
-    exam = int(input("Please enter their asessment score: "))
+    eScore = int(input("Please enter their asessment score: "))
 
-result = grading(hwscore, ascore, exam)
+result = grading(hScore, aScore, eScore)
 
 if result >= 80:
     grade = "A"
