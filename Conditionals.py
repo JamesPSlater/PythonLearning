4752caac5f6914d2686e63c7b73047f04c5a1e23
JamesPSlater <jamespaulslater@gmail.com>
2022-02-08 10:59:22 +0000

    Exercise variations

--- Conditionals.py
+++ Conditionals.py
@@ -1,7 +1,45 @@
-var1 = int(input("Hello please enter your score: "))
-var2 = input("Please enter a name: ")
+'''
+Create a new Python file and write a program that does the following:
 
-if var1 >50 and var2:
-    print(var2, "Greater than 50")
-else:
-    print("Less than 50")
+Asks for an input from the user as a mark.
+If the mark is greater that 85 print "Distinction"
+If the mark is between 65 and 85 print "Pass"
+Anything else print "Fail"
+Try to do this both with and without elif statements.
+'''
+
+# mark = int(input("Please enter your score: "))
+# if mark > 85:
+#     print("Distinction")
+# elif mark >= 65:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# mark = int(input("Please enter your mark: "))
+# if mark > 85:
+#     print("Distinction")
+# if 85 >= mark >= 65:
+#     print("Pass")
+# if mark < 65:
+#     print("Fail")
+
+# score = int(input("Enter your exam score "))
+# key = 0
+# if score > 85 : key = 1
+# if 85 >= score >= 65: key = 2
+# if score < 65 : key = 3
+# Exam_Score = {
+#                 1: "Distinction",
+#                 2: "Pass",
+#                 3: "Fail"
+#              }
+# print(Exam_Score.get(key))
+
+score = int(input("Enter your exam score "))
+key = []
+if score > 85 : key = 0
+if 85 >= score >= 65 : key = 1
+if score < 65 : key = 2
+Exam_Score = ["Distinction", "Pass", "Fail"]
+print(Exam_Score[key])
