75c6f7870ac2bc1ad2282576c070d29bfe4b875e
JamesPSlater <jamespaulslater@gmail.com>
2022-02-08 09:39:43 +0000

    Added Conditionals file

---
+++ Conditionals.py
@@ -0,0 +1,7 @@
+var1 = int(input("Hello please enter your score: "))
+var2 = input("Please enter a name: ")
+
+if var1 >50 and var2:
+    print(var2, "Greater than 50")
+else:
+    print("Less than 50")
