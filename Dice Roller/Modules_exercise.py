3adbb33df90eae27fb5642af7a1ae1ae575f897a
JamesPSlater <jamespaulslater@gmail.com>
2022-02-09 11:20:49 +0000

    Dice roller

---
+++ Dice Roller/Modules_exercise.py
@@ -0,0 +1,7 @@
+from dice import die    
+
+print(die())
+
+roll1, roll2 = die()
+print(roll1)
+print(roll2)
---
+++ Dice Roller/dice.py
@@ -0,0 +1,7 @@
+from random import randint
+
+def die():
+    var1 = randint(1, 6)
+    var2 = randint(1, 6)
+    list1 = [var1, var2] 
+    return list1
